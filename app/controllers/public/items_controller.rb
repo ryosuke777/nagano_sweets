51ee08af3a7fd1065ad0ab766670267a1326f007
vagrant <vagrant@localhost.localdomain>
2020-12-13 11:49:46 +0000

    change_image-url_to_asset_path

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -10,6 +10,7 @@ class Public::ItemsController < ApplicationController
     # @cart_items = current_cart.cart_items
     @genres = Genre.all
 	  @item = Item.find(params[:id])
+    # binding.pry
     @cart_item = CartItem.new
     # @cart_item.quantity += params[:quantity].to_i
 	end
